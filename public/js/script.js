813b0a1b826cd547b65dd1da3f36ac29adf34dde
Chris <chris@Chriss-iMac.local>
2017-09-21 15:37:35 +1200

    Pulling api data to get name and profile pic

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,31 @@
 var accessToken = "uLqA16H6sQWfTUFyQij6fsxlkDUSnckN";
 
-$.ajax({
-		url: "https://api.behance.net/v2/creativestofollow?client_id=uLqA16H6sQWfTUFyQij6fsxlkDUSnckN",
-		dataType: "jsonp",
-		success:function(dataFromBehance){
-			console.log(dataFromBehance);
+// $.ajax({
+// 		url: "https://api.behance.net/v2/creativestofollow?client_id=uLqA16H6sQWfTUFyQij6fsxlkDUSnckN",
+// 		dataType: "jsonp",
+// 		success:function(dataFromBehance){
+// 			console.log(dataFromBehance);
+//
+//
+// 		},
+//
+// 		error:function(){
+// 			console.log("can't connect to Behance api");
+// 		}
 
 
-		},
 
-		error:function(){
-			console.log("can't connect to Behance api");
-		}
-
-
-
-	});
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> d0308a03f34999c7e68292d219f27103b4663416
+	// });
 
 	$.ajax({
 
-			url: "http://www.behance.net/v2/users/EstudioPum/projects?client_id="+ accessToken,
+			url: "http://www.behance.net/v2/users/linnfritz?client_id="+ accessToken,
 			dataType: "jsonp",
 			success:function(dataFromBehance){
 				console.log(dataFromBehance);
-				console.log(dataFromBehance.projects[1].covers.original);
-
+				$("#user-name").append(dataFromBehance.user.display_name);
+				$("#display-pic").append("<img  src='"+dataFromBehance.user.images[230]+"'>");
+				// $("#user_name").append(dataFromBehance.user.display_name);
 
 			},
 
@@ -39,9 +35,6 @@ $.ajax({
 
 
 
+
+
 		});
-<<<<<<< HEAD
-=======
->>>>>>> master
-=======
->>>>>>> d0308a03f34999c7e68292d219f27103b4663416
