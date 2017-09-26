802c864cffdbdaf1dc6fbec1f3012b0be77d6cbd
constanza de los angeles magallon astudillo <845869265@ken20-202-09.local>
2017-09-27 09:16:56 +1300

    connie and kirsty pop up function

--- public/js/script-home.js
+++ public/js/script-home.js
@@ -34,24 +34,7 @@ function getProjectData(id){
 					"<img class='project-img' src='"+projectOne[a].covers['original']+"' />"+
 					"<div class='project-txt'>"+projectOne[a].name+"</div>"
 					+"</div>")
-
-				$('[data-id=' + id + '] .profile-project_single').click(function(){
-					var projectId = dataFromBehance.projects;
-					for (var i = 0; i < projectId.length; i++){
-						console.log(projectId[i].id);
-					}
-				});
-				// getProjectId();
 			};
-			// $('.profile-project_single').click(function(){
-			// 	console.log($(this));
-				// var projectID = $(this).projectOne[a].id;
-				// var projectID = $(this)[0].dataset.projectsid;
-				// console.log($(this)[0].dataset.projectsid);
-				// console.log(projectID);
-				// console.log(dataFromBehance.projects[1].id);
-				// console.log(dataFromBehance.projects[2].id);
-			// })
 		},
 		error:function(){
 			console.log("can't connect to Behance api");
@@ -59,33 +42,6 @@ function getProjectData(id){
 	});
 }
 
-// function getProjectId(){
-// 	$.ajax({
-// 		url: "https://www.behance.net/v2/users/"+id+"/projects?client_id=" + accessToken,
-// 		dataType: "jsonp",
-// 		success:function(dataFromBehance){
-// 			$('[data-id=' + id + '] .profile-project_single').click(function(){
-// 				var projectId = dataFromBehance.projects;
-// 				for (var i = 0; i < projectId.length; i++){
-// 					console.log(projectId[i].id);
-// 				}
-// 			});
-// 			// console.log(dataFromBehance);
-// 			// $('[data-id=' + id + '] .profile-project_single').click(function(){
-// 			// 	// console.log($(this));
-// 			// })
-
-// 		},
-// 		error:function(){
-// 			console.log("can't connect to Behance api");
-// 		}
-// 	});
-// }
-
-// function getProjectID(){
-	
-// }
-
-
-
-
+$(document).on('click', '.profile-project_single', function(e) {
+	console.log($(this)[0].dataset.id);
+});
