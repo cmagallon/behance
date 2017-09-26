10bebda98f03f75dc0fae276288db4b9e82334df
constanza de los angeles magallon astudillo <845869265@ken20-202-09.local>
2017-09-26 20:39:55 +1300

    connie and kirst merge

--- public/js/script-home.js
+++ public/js/script-home.js
@@ -1,4 +1,4 @@
-var accessToken = "kPjbO9ychhcXHOcnuBdYaUtWryHz5a5G";
+var accessToken = "uLqA16H6sQWfTUFyQij6fsxlkDUSnckN";
 
 $.ajax({
 	url: "https://api.behance.net/v2/creativestofollow?client_id=" + accessToken,
@@ -22,7 +22,7 @@ $.ajax({
 });
 
 function getProjectData(id){
-	console.log(id);
+	// console.log(id);
 	$.ajax({
 		url: "https://www.behance.net/v2/users/"+id+"/projects?client_id=" + accessToken +"&per_page=3",
 		dataType: "jsonp",
@@ -30,14 +30,62 @@ function getProjectData(id){
 			var projectOne = dataFromBehance.projects;
 			for (var a = 0; a < projectOne.length; a++) {
 				// console.log(a);
-				$('[data-id=' + id + '] .projects').append("<div class='profile-project_single'>"+
+				$('[data-id=' + id + '] .projects').append("<div data-id='"+projectOne[a].id+"' class='profile-project_single'>"+
 					"<img class='project-img' src='"+projectOne[a].covers['original']+"' />"+
 					"<div class='project-txt'>"+projectOne[a].name+"</div>"
 					+"</div>")
+
+				$('[data-id=' + id + '] .profile-project_single').click(function(){
+					var projectId = dataFromBehance.projects;
+					for (var i = 0; i < projectId.length; i++){
+						console.log(projectId[i].id);
+					}
+				});
+				// getProjectId();
 			};
+			// $('.profile-project_single').click(function(){
+			// 	console.log($(this));
+				// var projectID = $(this).projectOne[a].id;
+				// var projectID = $(this)[0].dataset.projectsid;
+				// console.log($(this)[0].dataset.projectsid);
+				// console.log(projectID);
+				// console.log(dataFromBehance.projects[1].id);
+				// console.log(dataFromBehance.projects[2].id);
+			// })
 		},
 		error:function(){
 			console.log("can't connect to Behance api");
 		}
 	});
 }
+
+// function getProjectId(){
+// 	$.ajax({
+// 		url: "https://www.behance.net/v2/users/"+id+"/projects?client_id=" + accessToken,
+// 		dataType: "jsonp",
+// 		success:function(dataFromBehance){
+// 			$('[data-id=' + id + '] .profile-project_single').click(function(){
+// 				var projectId = dataFromBehance.projects;
+// 				for (var i = 0; i < projectId.length; i++){
+// 					console.log(projectId[i].id);
+// 				}
+// 			});
+// 			// console.log(dataFromBehance);
+// 			// $('[data-id=' + id + '] .profile-project_single').click(function(){
+// 			// 	// console.log($(this));
+// 			// })
+
+// 		},
+// 		error:function(){
+// 			console.log("can't connect to Behance api");
+// 		}
+// 	});
+// }
+
+// function getProjectID(){
+	
+// }
+
+
+
+
