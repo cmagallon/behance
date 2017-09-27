e68de99928671619cde3cae2d001e76398432acc
kirsten opie <845841129@ken20-202-15.local>
2017-09-28 09:34:17 +1300

    made changes to append data to profile module onclick

--- public/js/script-home.js
+++ public/js/script-home.js
@@ -1,4 +1,3 @@
-
 $.ajax({
 			url: "./config.json",
 			dataType: "json",
@@ -6,7 +5,6 @@ $.ajax({
 			if (xhr.overrideMimeType) {
 			xhr.overrideMimeType("application/json");
 			}
-
 		},
 			success:function(data){
 				console.log("workingTest")
@@ -16,14 +14,12 @@ $.ajax({
 				getData();
 
 			},
-
 		error:function(){
 			console.log("can't connect to Behance api");
 		}
 
+});
 
-
-	});
 function getData(){
 
 $.ajax({
@@ -66,20 +62,19 @@ function getProjectData(id){
 			console.log("can't connect to Behance api");
 		}
 	});
-}
+};
 
-$(document).on('click', '.profile-project_single', function(e) {
+$(document).on('click', '.profile-project_single', function(e){
 	// console.log($(this)[0].dataset.id);
 	var projectId = $(this)[0].dataset.id;
 	console.log(projectId);
 
 	$.ajax({
-
-	url: "https://api.behance.net/v2/projects/"+ projectId +"?api_key=" + key,
-	dataType: "jsonp",
-	success:function(dataFromBehance){
+		url: "https://api.behance.net/v2/projects/"+ projectId +"?api_key=" + accessToken,
+		dataType: "jsonp",
+		success:function(dataFromBehance){
 		console.log(dataFromBehance);
-
+		
 		var projectName = dataFromBehance.project.name;
 		console.log(projectName);
 		var fields = dataFromBehance.project.fields;
@@ -90,43 +85,44 @@ $(document).on('click', '.profile-project_single', function(e) {
 		console.log(likeNum);
 		var projectImage = dataFromBehance.project.covers['original'];
 		console.log(projectImage);
-
-
-
+		
 
 		// Code for popup profile
 
-// Get the modal
-	var modal = document.getElementById('projectModal');
-	modal.style.display = "block";
+		// Get the modal
+		var modal = document.getElementById('projectModal');
+		modal.style.display = "block";
 
-	//image
-	// get getElementById
-	// remove currnet src
-	//add new
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		    if (event.target == modal) {
+		        modal.style.display = "none";
+		    }
+		}
 
+		var span = document.getElementsByClassName("closeProject")[0];
+		span.onclick = function() {
 
+		    modal.style.display = "none";
+		    $("#project-name").empty("<p>" +dataFromBehance.project.name + "</p>");
+		    $("#fields").empty("<p>" +dataFromBehance.project.fields + "</p>");
+			$("#view-num").empty("<p>" +dataFromBehance.project.stats.views + "</p>");
+			$("#like-num").empty("<p>" +dataFromBehance.project.stats.appreciations + "</p>");
+			$("#img1").empty("<p><img src='" +dataFromBehance.project.covers["original"] + "'></p>");
+		
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+			$("#project-name").append("<p>" +dataFromBehance.project.name + "</p>");
+			$("#fields").append("<p>" +dataFromBehance.project.fields + "</p>");
+			$("#view-num").append("<p>" +dataFromBehance.project.stats.views + "</p>");
+			$("#like-num").append("<p>" +dataFromBehance.project.stats.appreciations + "</p>");
+			$("#img1").append("<p><img src='" +dataFromBehance.project.covers["original"] + "'></p>");
 
-var span = document.getElementsByClassName("closeProject")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-}
+		},
+		error:function(){
+			console.log("can't connect to Behance api");
+		}
+	});
 
-	},
-	error:function(){
-		console.log("can't connect to Behance api");
-	}
 });
-
-});
-
-}
 
 // When the user clicks on <span> (x), close the modal
