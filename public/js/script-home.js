ad21b426d900cb5ce512e7db255a4e8d1bc70a69
kirsten opie <845841129@ken20-202-15.local>
2017-09-28 09:48:52 +1300

    pushing modal popup to master

--- public/js/script-home.js
+++ public/js/script-home.js
@@ -1,47 +1,47 @@
 $.ajax({
-			url: "./config.json",
-			dataType: "json",
-			beforeSend: function(xhr) {
-			if (xhr.overrideMimeType) {
-			xhr.overrideMimeType("application/json");
-			}
-		},
-			success:function(data){
-				console.log("workingTest")
-				key = data.APIkey;
+	url: "./config.json",
+	dataType: "json",
+	beforeSend: function(xhr) {
+	if (xhr.overrideMimeType) {
+	xhr.overrideMimeType("application/json");
+	}
+},
+	success:function(data){
+		console.log("workingTest")
+		key = data.APIkey;
 
 
-				getData();
+		getData();
 
-			},
-		error:function(){
-			console.log("can't connect to Behance api");
-		}
+	},
+error:function(){
+	console.log("can't connect to Behance api");
+}
 
 });
 
 function getData(){
-
-$.ajax({
-	url: "https://api.behance.net/v2/creativestofollow?client_id=" + key,
-	dataType: "jsonp",
-	success:function(dataFromBehance){
-		var creatives = dataFromBehance.creatives_to_follow;
-		for (var i = 0; i < creatives.length; i++) {
-			// console.log(creatives[i]);
-			$("#behanceUsers").append("<div data-id='"+creatives[i].id+"'class='profile'>"+
-				"<div class='profile-pic'>"+ "<img class='img-circle' src='"+
-				creatives[i].images[138]+"'/>"+
-				"<h2 class='profile-name'>"+creatives[i].display_name+"</h2>"+
-				"<h3 class='profile-desc'>"+creatives[i].occupation+"</h3>"
-				+"</div>"+"<div class='projects'></div>"+"</div>")
-			getProjectData(creatives[i].id);
-		};
-	},
-	error:function(){
-		console.log("can't connect to Behance api");
-	}
-});
+	$.ajax({
+		url: "https://api.behance.net/v2/creativestofollow?client_id=" + key,
+		dataType: "jsonp",
+		success:function(dataFromBehance){
+			var creatives = dataFromBehance.creatives_to_follow;
+			for (var i = 0; i < creatives.length; i++) {
+				// console.log(creatives[i]);
+				$("#behanceUsers").append("<div data-id='"+creatives[i].id+"'class='profile'>"+
+					"<div class='profile-pic'>"+ "<img class='img-circle' src='"+
+					creatives[i].images[138]+"'/>"+
+					"<h2 class='profile-name'>"+creatives[i].display_name+"</h2>"+
+					"<h3 class='profile-desc'>"+creatives[i].occupation+"</h3>"
+					+"</div>"+"<div class='projects'></div>"+"</div>")
+				getProjectData(creatives[i].id);
+			};
+		},
+		error:function(){
+			console.log("can't connect to Behance api");
+		}
+	});
+}
 
 function getProjectData(id){
 	// console.log(id);
@@ -62,17 +62,18 @@ function getProjectData(id){
 			console.log("can't connect to Behance api");
 		}
 	});
-};
+}
 
-$(document).on('click', '.profile-project_single', function(e){
+$(document).on('click', '.profile-project_single', function(e) {
 	// console.log($(this)[0].dataset.id);
 	var projectId = $(this)[0].dataset.id;
 	console.log(projectId);
 
 	$.ajax({
-		url: "https://api.behance.net/v2/projects/"+ projectId +"?api_key=" + accessToken,
-		dataType: "jsonp",
-		success:function(dataFromBehance){
+	
+	url: "https://api.behance.net/v2/projects/"+ projectId +"?api_key=" + key,
+	dataType: "jsonp",
+	success:function(dataFromBehance){
 		console.log(dataFromBehance);
 		
 		var projectName = dataFromBehance.project.name;
@@ -89,39 +90,43 @@ $(document).on('click', '.profile-project_single', function(e){
 
 		// Code for popup profile
 
-		// Get the modal
-		var modal = document.getElementById('projectModal');
-		modal.style.display = "block";
+// Get the modal
+	var modal = document.getElementById('projectModal');
+	modal.style.display = "block";
 
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-		    if (event.target == modal) {
-		        modal.style.display = "none";
-		    }
-		}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
-		var span = document.getElementsByClassName("closeProject")[0];
-		span.onclick = function() {
+var span = document.getElementsByClassName("closeProject")[0];
+span.onclick = function() {
 
-		    modal.style.display = "none";
-		    $("#project-name").empty("<p>" +dataFromBehance.project.name + "</p>");
-		    $("#fields").empty("<p>" +dataFromBehance.project.fields + "</p>");
-			$("#view-num").empty("<p>" +dataFromBehance.project.stats.views + "</p>");
-			$("#like-num").empty("<p>" +dataFromBehance.project.stats.appreciations + "</p>");
-			$("#img1").empty("<p><img src='" +dataFromBehance.project.covers["original"] + "'></p>");
-		
+    modal.style.display = "none";
+    $("#project-name").empty("<p>" +dataFromBehance.project.name + "</p>");
+    $("#fields").empty("<p>" +dataFromBehance.project.fields + "</p>");
+	$("#view-num").empty("<p>" +dataFromBehance.project.stats.views + "</p>");
+	$("#like-num").empty("<p>" +dataFromBehance.project.stats.appreciations + "</p>");
+	$("#img1").empty("<p><img src='" +dataFromBehance.project.covers["original"] + "'></p>");
 
-			$("#project-name").append("<p>" +dataFromBehance.project.name + "</p>");
-			$("#fields").append("<p>" +dataFromBehance.project.fields + "</p>");
-			$("#view-num").append("<p>" +dataFromBehance.project.stats.views + "</p>");
-			$("#like-num").append("<p>" +dataFromBehance.project.stats.appreciations + "</p>");
-			$("#img1").append("<p><img src='" +dataFromBehance.project.covers["original"] + "'></p>");
 
-		},
-		error:function(){
-			console.log("can't connect to Behance api");
-		}
-	});
+
+}
+
+$("#project-name").append("<p>" +dataFromBehance.project.name + "</p>");
+$("#fields").append("<p>" +dataFromBehance.project.fields + "</p>");
+$("#view-num").append("<p>" +dataFromBehance.project.stats.views + "</p>");
+$("#like-num").append("<p>" +dataFromBehance.project.stats.appreciations + "</p>");
+$("#img1").append("<p><img src='" +dataFromBehance.project.covers["original"] + "'></p>");
+
+
+	},
+	error:function(){
+		console.log("can't connect to Behance api");
+	}
+});
 
 });
 
